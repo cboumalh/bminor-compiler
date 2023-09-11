1efb854e6c2590c948dc60ea95958213a0a29496
Christopher Boumalhab <cboumalh@nd.edu>
2023-09-11 02:50:27 -0400

    added test cases

---
+++ scan.h
@@ -0,0 +1,21 @@
+#ifndef SCAN_H
+#define SCAN_H
+
+#include "lex.yy.c"
+#include <stdlib.h>
+#include "scan.h"
+#include "token.h"
+
+int scan(FILE* file){
+    yyin = file;
+    while(1){
+        int token = yylex();
+        if(!token) break;
+        if (token == ERROR_TOKEN) return EXIT_FAILURE;
+    }
+    
+    return EXIT_SUCCESS;
+}
+
+
+#endif
