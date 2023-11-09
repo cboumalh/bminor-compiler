8351a095ed869bad9b7e5b0613ee8fb98330ad76
Christopher Boumalhab <cboumalh@nd.edu>
2023-11-09 18:55:24 -0500

    temp removed typechecker tests

--- runtest.sh
+++ runtest.sh
@@ -114,24 +114,3 @@ do
 		echo "$testfile failure (as expected)"
 	fi
 done
-
-#typechecker
-for testfile in ./test/typecheck/good*.bminor
-do
-	if ./bminor --typecheck $testfile > $testfile.out
-	then
-		echo "$testfile success (as expected)"
-	else
-		echo "$testfile failure (INCORRECT)"
-	fi
-done
-
-for testfile in ./test/typecheck/bad*.bminor
-do
-	if ./bminor --typecheck $testfile > $testfile.out
-	then
-		echo "$testfile success (INCORRECT)"
-	else
-		echo "$testfile failure (as expected)"
-	fi
-done
